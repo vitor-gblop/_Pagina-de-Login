aff7dc8cabca31a8d0542977c08827162a7963e7
br_364 <134791002+vitor-gblop@users.noreply.github.com>
2024-06-24 11:49:16 -0300

    Update authApp.js
    Atualização da documentação do código

--- _forms/html_config/authApp.js
+++ _forms/html_config/authApp.js
@@ -3,22 +3,24 @@
 /*
     Funcoes de envio de email e estetica
 
-    *OBS: serviços de email comentados(linha 45), defina de onde sera enviado o email(gmail)(linha 34) e depois ative o serviço de envio
+    *OBS: serviços de email estão comentados, defina o email de onde será enviado o email(gmail) e depois ative a API de envio de emails.
 
-    linha 10 - função de confirmação de email
+    Função - confirmMail
     {
-        Recebe os campos de senha, confirmação de senha, nome e de email.(linhas 29 a 34)
-        Verifica se os valores digitados no campo de senha, email, nome estao corretos(linha 36)
-        Verifica se os valores digitados no campo de senha e confirmar senha da pagina signin(cadastro) sao iguais(linha 38).
+        Recebe os campos de senha, confirmação de senha, nome e de email.
+        Verifica se os valores digitados no campo de senha, email, nome estao corretos
+        Verifica se os valores digitados no campo de senha e confirmar senha da pagina signin(cadastro) sao iguais.
 
-        Se sim inicia o serviço da api do emailjs (necessita de cadastro)(linha 40), gera um numero aleatorio entre 0 e 999 para ser enviado(linha 42), define os parametros da mensagem(linhas 44 - 50), realiza configurações esteticas da pagina(linhas 52 e 53) e finalmente envia o email com os parametros passados(linha 56)
+        Se sim inicia o serviço da api do emailjs (necessita de cadastro), gera um numero aleatorio entre 0 e 999 para ser enviado, define os parametros da mensagem, realiza configurações esteticas da pagina e finalmente envia o email com os parametros passados
     }
 
-    linha 68 - função de verificação do codigo
+    Função - secondStepVerification
     {
-        Recebe o valor digitado no campo 'numero de confirmação'(linha 72).
-        Logo apos verifica se o valor e igual a variavel random que foi enviada ao email do usuario(linha 74), se sim ativa o botao se submit para finalizar o cadastro senao nao faz nada.
+        Recebe o valor digitado no campo 'numero de confirmação'.
+        Logo apos verifica se o valor e igual a variavel random que foi enviada ao email do usuario, se sim ativa o botao se submit para finalizar o cadastro senão, ignora.
     }
+
+    Função - 
 */
 
 let random;
@@ -26,21 +28,26 @@ function confirmMail()
 {   
     let re = /[0-9]*5/g
 
+    // armazena os inputs de senha e confirmação de senha num array
     const passes = [];
     passes[0] = document.getElementById("senha_1");
     passes[1] = document.getElementById("senha_2");
 
+    // armazena nome e email nas respectivas variaveis, nome e email
     let nome = document.getElementById("user_input").value;
     let email = document.getElementById("email_input").value;
 
+    // testa os valores dos inputs para 
     if(re.test(passes[0].value) && email != "" && nome != "")
     {
         if (passes[0].value == passes[1].value)
         {
-            emailjs.init("0caVJ0GwvPT-G52t2");
+            // emailjs.init("0caVJ0GwvPT-G52t2");
+            // chave de inicialização da API 
 
             random = Math.floor(Math.random() * 1000);
-            
+
+            // define os parametros da mensagem
             let param = {
                 from_email: "", // possivel erro caso nao definido.
                 from_name: "Autenticador web",
@@ -49,11 +56,14 @@ function confirmMail()
                 to: email
             }
 
+            // ???
             document.getElementById("signin_email_div").style.display = "flex";
             document.getElementById("confirm_data").style.display = "none";
 
-            // -------("chave publica da api", "modelo do email", parametros)
+            // envia o email
+            // --("chave publica da api", "modelo do email", parametros)--
             // emailjs.send("service_1mtbxqi","template_rtkmmgx",param)
+            
             // .then(res => {
             //     window.alert("email sent succesfully");
             // });
@@ -69,11 +79,15 @@ function confirmMail()
 function secondStepVerification()
 {
     console.log(random);
+    // recebe o valor do campo de confirmação do email(codigo enviado no email)
     let a = document.getElementById("second_auth_step").value;
 
+    // compara o código recebido com o que foi enviado.
     if (a == random)
     {
         let b = document.getElementById("end_submit");
+
+        // ativa o botao de submit e permite o cadstro ou login
         b.disabled = false;
         // alert("ativa o submit");
     }
